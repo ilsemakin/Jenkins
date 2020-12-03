d0cbf552243d07d3708d42d6a000eb503273fc31
Ilya Semakin <il.semakin@mail.ru>
2020-12-03 15:59:10 +0300

    Changing the position of the brackets

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -33,8 +33,8 @@ public class AggregatorTest {
         Aggregator agr = new Aggregator();
         double val = 1;
         agr.addValue(val);
-        assertThat(agr.getSum().isEqualTo(val));
+        assertThat(agr.getSum()).isEqualTo(val);
         agt.reset();
-        assertThat(agr.getSum().isEqualTo(0));
+        assertThat(agr.getSum()).isEqualTo(0);
     }
 }
